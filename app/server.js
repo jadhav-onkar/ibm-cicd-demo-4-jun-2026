ffee8b40aca5c3742850d31304c6904c2577c09e
Onkar Jadhav <ganeshjadhav7478@gmail.com>
2026-07-16 10:00:50 +0530

    change

--- app/server.js
+++ app/server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
   console.log('done');
-  res.json({ status: 'UP', message: "hi ganesh" });
+  res.json({ status: 'UP', message: "hi aman" });
 });
 
 // ESM equivalent of `if (require.main === module)` — only start the
